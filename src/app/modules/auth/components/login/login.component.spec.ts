3ebd8cd8eb2156511df055ba432fc3cc51ca0ddb
Jose Ramirez <jcramireztello@gmail.com>
2021-01-08 18:44:36 -0500

    feat: add jasmine test

--- src/app/modules/auth/components/login/login.component.spec.ts
+++ src/app/modules/auth/components/login/login.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { LoginComponent } from './login.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,6 +10,11 @@ describe('LoginComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        HttpClientModule,
+        ReactiveFormsModule,
+      ],
       declarations: [ LoginComponent ]
     })
     .compileComponents();
@@ -22,4 +29,28 @@ describe('LoginComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('form should be valid', () => {
+    component.loginForm.controls['username'].setValue('admin');
+    component.loginForm.controls['password'].setValue('peru2018');
+    fixture.detectChanges();
+    expect(component.loginForm.valid).toEqual(true);
+  });
+
+  it('form should be invalid', () => {
+    component.loginForm.controls['username'].setValue('admin');
+    component.loginForm.controls['password'].setValue('');
+    fixture.detectChanges();
+    expect(component.loginForm.invalid).toEqual(true);
+  });
+
+  it('form submit create token', () => {
+    component.loginForm.controls['username'].setValue('admin');
+    component.loginForm.controls['password'].setValue('peru2018');
+    fixture.detectChanges();
+    component.onSubmit();
+    const token = localStorage.getItem('sessionToken');
+    expect(token).not.toBeUndefined(token);
+  });
+
 });
