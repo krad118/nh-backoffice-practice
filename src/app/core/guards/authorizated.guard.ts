f5019132d4b8523ad653e03eeb42719bf213b9eb
Jose Ramirez <jcramireztello@gmail.com>
2021-01-08 15:05:45 -0500

    feat: guards authorizated use token

--- src/app/core/guards/authorizated.guard.ts
+++ src/app/core/guards/authorizated.guard.ts
@@ -1,17 +1,30 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthorizatedStorageService } from '../services/authorizated-storage.service';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthorizatedGuard implements CanActivate {
+  constructor(
+    private router: Router,
+    private authorizatedStorage: AuthorizatedStorageService
+  ) {}
   canActivate(
     // validar si el token es valido 
     // roles y permisos
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.hasAccesss();
+  }
+
+  hasAccesss(): boolean {
+    if (this.authorizatedStorage.getTokenStorage()) {
+      return true;
+    }
+    this.router.navigate(['/auth/login/'])
     return false;
   }
-  
 }
